7acde1e3d054c769368fb335c7622590f19c2df6
devendra-dantal04 <devendra.dantal04@gmail.com>
2023-11-24 23:12:03 +0530

    Main, About Section Completed ✅

---
+++ script.js
@@ -0,0 +1,19 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true
+// });
+
+
+// let navSection = document.querySelector('.nav__section__wrapper');
+
+// function scrollTop() {
+//     if (window.scrollY > 20) {
+//         navSection.style.background = "white"
+//     } else {
+//         navSection.style.background = "#1e1e1e"
+//     }
+
+//     console.log(window.scrollY)
+// }
+
+// window.addEventListener('scroll', scrollTop);
